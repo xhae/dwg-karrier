45af6dd055aefb0a493f127a836d557459215883
leesera <qseralee@gmail.com>
2017-01-19 22:49:38 +0900

    Format the access token and user id name based on camel case

--- Roys/app/src/main/java/com/dwg_karrier/roys/MainActivity.java
+++ Roys/app/src/main/java/com/dwg_karrier/roys/MainActivity.java
@@ -6,8 +6,8 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-  final String accessToken = "A06EprS0187tNdGMJ1XPTVQa1eE8SeGLXZeK3GZy2UwZ8qzOGSqZlPmXNcYul0zueeQRLYwN1nWbFszj6PyoNOkCGSbUp9zfJ3eLROo3bJWsUQktkXPfbFruJn9TGFQQ5r16aLhP7f-VXMFNxMtlrJw21eabhWzhzO-9r0OkXBesU_0Kscpb4SaRPW4TpYpfGiusnAKhaWmeNYdu5VaCGMdFpoch:feedlydev";
-  final String id = "3d0c7dd1-a7bb-4cdf-92f0-6c25d88c52db";
+  static final String ACCESS_TOKEN = "A06EprS0187tNdGMJ1XPTVQa1eE8SeGLXZeK3GZy2UwZ8qzOGSqZlPmXNcYul0zueeQRLYwN1nWbFszj6PyoNOkCGSbUp9zfJ3eLROo3bJWsUQktkXPfbFruJn9TGFQQ5r16aLhP7f-VXMFNxMtlrJw21eabhWzhzO-9r0OkXBesU_0Kscpb4SaRPW4TpYpfGiusnAKhaWmeNYdu5VaCGMdFpoch:feedlydev";
+  static final String ID = "3d0c7dd1-a7bb-4cdf-92f0-6c25d88c52db";
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
