43bf666b07fa694112947493a1a1f6fa15bc4dd1
leesera <qseralee@gmail.com>
2017-01-19 14:51:07 +0900

    Add developer access token and id

--- Roys/app/src/main/java/com/dwg_karrier/roys/MainActivity.java
+++ Roys/app/src/main/java/com/dwg_karrier/roys/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+  final String accessToken = "A06EprS0187tNdGMJ1XPTVQa1eE8SeGLXZeK3GZy2UwZ8qzOGSqZlPmXNcYul0zueeQRLYwN1nWbFszj6PyoNOkCGSbUp9zfJ3eLROo3bJWsUQktkXPfbFruJn9TGFQQ5r16aLhP7f-VXMFNxMtlrJw21eabhWzhzO-9r0OkXBesU_0Kscpb4SaRPW4TpYpfGiusnAKhaWmeNYdu5VaCGMdFpoch:feedlydev";
+  final String id = "3d0c7dd1-a7bb-4cdf-92f0-6c25d88c52db";
+  
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
