422d9825723e25539378c128fb7dd6aaeafdfa18
Csoyee <ithdi@naver.com>
2017-02-01 00:34:20 +0900

    merge

--- Roys/app/src/main/java/com/dwg_karrier/roys/ContentView.java
+++ Roys/app/src/main/java/com/dwg_karrier/roys/ContentView.java
@@ -3,55 +3,65 @@ package com.dwg_karrier.roys;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
-import android.widget.TextView;
-
-import java.util.Date;
+import android.widget.Toast;
 
 public class ContentView extends AppCompatActivity {
+  private final String imgSizeCtrl = "<style>img{display: inline; height: auto; max-width: 100%;}</style>\n"; // fit image to the size of viewer
+  String title;
+  String content;
+  String url;
+  long startTime;
+  long endTime;
+  /*
+   * TODO(Juung): get CurTime and finTime so that could use them to calculate rest of the time --> use in next recommendation
+   * TODO: think about 'go-back' action (should go back to the first page or the second?)
+   */
 
-  @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.content);
 
-    final Intent intent = new Intent(this.getIntent());
-    final Date finTime = (Date) intent.getSerializableExtra("finTime");
-    final Date curTime = new Date(System.currentTimeMillis());
+    final Intent getPageInfo = new Intent(this.getIntent());
+    title = getPageInfo.getStringExtra("title");
+    content = getPageInfo.getStringExtra("content");
+    String view = title + "\n\n" + imgSizeCtrl + content;
 
-    try {
+    WebView wv = (WebView) findViewById(R.id.contentView);
       /*
-       * TODO
-       * bring user-clicked URL
-       * merge with soyee's code
+       * viewer settings
+       * v.2 might have image scaling (Let's talk about this after using v.1)
        */
-      // yet, write exact pageUrl
-      String pageUrl = "http://www.bloter.net/archives/265787";
-      Crawler crawler = new Crawler(pageUrl);
-      // show title and content in one page
-      // It's better to scroll down in concatenated version
-      String title = crawler.getTitle();
-      String content = crawler.getContent();
-      TextView contentView = (TextView) findViewById(R.id.contentView);
-      contentView.setText(title + "\n\n" + content);
-      contentView.setMovementMethod(new ScrollingMovementMethod());
-    } catch (Exception e) {
-      Log.e("Error:", e.getMessage(), e);
-      e.printStackTrace();
-    }
+    wv.setVerticalScrollBarEnabled(true);
+    wv.setHorizontalScrollBarEnabled(false);
 
-    Button b = (Button) findViewById(R.id.button4);
-    b.setOnClickListener(new View.OnClickListener() {
+    final String mimeType = "text/html";
+    final String encoding = "UTF-8";
+    wv.loadDataWithBaseURL("", view, mimeType, encoding, "");
+
+    startTime = System.currentTimeMillis();
+
+    Button finishReading = (Button) findViewById(R.id.buttonFinish);
+    url = getPageInfo.getStringExtra("url");
+    finishReading.setOnClickListener(new View.OnClickListener() {
+
       @Override
       public void onClick(View v) {
-        Intent intent1 = new Intent(ContentView.this, ListActivity.class);
-        intent1.putExtra("finTime", finTime);
-        intent1.putExtra("curTime", curTime);
-        startActivity(intent1);
-        finish();
+        endTime = System.currentTimeMillis();
+        DataBaseOpenHelper dbHelper = new DataBaseOpenHelper(ContentView.this);
+        dbHelper.setIsRead(url, 1);
+
+        // Temporal check for DB and read time
+        long readTime = (endTime - startTime) / 1000;
+        Log.d("readTime", String.valueOf(readTime));
+        Toast checkInfo = Toast.makeText(getApplicationContext(), "Congratulations!" + "\n" +
+            "You finished reading in " + String.valueOf(readTime) + "sec", Toast.LENGTH_LONG);
+        checkInfo.setGravity(Gravity.BOTTOM, 0, 0);
+        checkInfo.show();
       }
     });
   }
