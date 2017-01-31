a4c5085f35464907444c61d3fb55c9b63a8eaabf
leesera <qseralee@gmail.com>
2017-01-31 19:23:33 +0900

    Remove empty lines

--- Roys/app/src/main/java/com/dwg_karrier/roys/ListActivity.java
+++ Roys/app/src/main/java/com/dwg_karrier/roys/ListActivity.java
@@ -57,7 +57,7 @@ public class ListActivity extends AppCompatActivity {
     for (ScriptedURL temp : wholeList) {
       tempTime = (double) temp.getWordCount() / wordsperMin;
       Log.d("test tempTime", "" + tempTime + "wordCount" + temp.getWordCount());
-      ret.add(new ScriptedData("asdf", tempTime, "ada"));
+      ret.add(new ScriptedData("", tempTime, ""));
     }
 
     return ret;
--- Roys/app/src/main/java/com/dwg_karrier/roys/MainActivity.java
+++ Roys/app/src/main/java/com/dwg_karrier/roys/MainActivity.java
@@ -29,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
   static final String ACCESS_TOKEN = "A06EprS0187tNdGMJ1XPTVQa1eE8SeGLXZeK3GZy2UwZ8qzOGSqZlPmXNcYul0zueeQRLYwN1nWbFszj6PyoNOkCGSbUp9zfJ3eLROo3bJWsUQktkXPfbFruJn9TGFQQ5r16aLhP7f-VXMFNxMtlrJw21eabhWzhzO-9r0OkXBesU_0Kscpb4SaRPW4TpYpfGiusnAKhaWmeNYdu5VaCGMdFpoch:feedlydev";
   static final String ID = "3d0c7dd1-a7bb-4cdf-92f0-6c25d88c52db";
 
-
   private static String convertStreamToString(InputStream is) {
 
     BufferedReader reader = new BufferedReader(new InputStreamReader(is));
@@ -62,13 +61,11 @@ public class MainActivity extends AppCompatActivity {
     Button btnFeedlyAccount = (Button) findViewById(R.id.FeedlyAccountBtn);
     btnFeedlyAccount.setOnClickListener(new Button.OnClickListener() {
       public void onClick(View v) {
-
         DataBaseOpenHelper dataBaseOpenHelper;
         dataBaseOpenHelper = new DataBaseOpenHelper(mainActivity);
 
         final String URL = "https://cloud.feedly.com/v3/streams/contents?streamId=user/" + ID + "/category/global.all";
         new GetPageList(dataBaseOpenHelper, mainActivity).execute(URL);
-
       }
     });
 
