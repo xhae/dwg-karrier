03c01d53de3361d7cfc9f63c96d463a3b4d1d088
Csoyee <ithdi@naver.com>
2017-02-10 00:25:56 +0900

    remove unused function from main

--- Roys/app/src/main/java/com/dwg_karrier/roys/MainActivity.java
+++ Roys/app/src/main/java/com/dwg_karrier/roys/MainActivity.java
@@ -3,7 +3,6 @@ package com.dwg_karrier.roys;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Point;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
@@ -14,65 +13,28 @@ import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-  static final String ACCESS_TOKEN = "A06EprS0187tNdGMJ1XPTVQa1eE8SeGLXZeK3GZy2UwZ8qzOGSqZlPmXNcYul0zueeQRLYwN1nWbFszj6PyoNOkCGSbUp9zfJ3eLROo3bJWsUQktkXPfbFruJn9TGFQQ5r16aLhP7f-VXMFNxMtlrJw21eabhWzhzO-9r0OkXBesU_0Kscpb4SaRPW4TpYpfGiusnAKhaWmeNYdu5VaCGMdFpoch:feedlydev";
-  static final String ID = "3d0c7dd1-a7bb-4cdf-92f0-6c25d88c52db";
   boolean connectAccount = false; // check if user connect to Account.
-
-  private static String convertStreamToString(InputStream is) {
-
-    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-    StringBuilder sb = new StringBuilder();
-
-    String line = null;
-    try {
-      while ((line = reader.readLine()) != null) {
-        sb.append(line + "\n");
-      }
-    } catch (IOException e) {
-      e.printStackTrace();
-    } finally {
-      try {
-        is.close();
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
-    return sb.toString();
-  }
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     setImage();
-
     final GridLayout minuteLayout = (GridLayout)findViewById(R.id.maingridLayout);
-
-    setImage();
 
     int childCount = minuteLayout.getChildCount();
     final int timeUnit = 10;
+
     for (int i = 0 ; i < childCount ; i++) {
       final ImageView container = (ImageView) minuteLayout.getChildAt(i);
       container.setTag((i + 1) * timeUnit + "");
       container.setOnClickListener(new View.OnClickListener() {
         public void onClick(View view) {
-          if(connectAccount == false) {
+          if(!connectAccount) {
             Toast checkInfo = Toast.makeText(getApplicationContext(), "Please connect to account.", Toast.LENGTH_SHORT);
             checkInfo.setGravity(Gravity.CENTER, 0, 0);
             checkInfo.show();
@@ -107,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
     Authentication authentication = new Authentication(mainActivity);
     authentication.authenticationAndBringPages();
+    connectAccount = true;
     return super.onOptionsItemSelected(item);
   }
 
