a693d3b2218f1eac958f4f34ea6a9a83162ef1c2
sunju0123 <sunjuhwang01@gmail.com>
2017-02-17 02:31:53 +0900

    change get data query, remove crawler

--- Roys/app/src/main/java/com/dwg_karrier/roys/DataBaseOpenHelper.java
+++ Roys/app/src/main/java/com/dwg_karrier/roys/DataBaseOpenHelper.java
@@ -85,7 +85,10 @@ public class DataBaseOpenHelper extends SQLiteOpenHelper {
 
     Cursor cursor = dataBase.rawQuery(query, null);
     while (cursor.moveToNext()) {
-      ScriptedURL scriptedItem = new ScriptedURL(cursor.getInt(readColumn), cursor.getString(titleColumn), cursor.getString(contentColumn), cursor.getInt(expectedTimeColumn));
+      ScriptedURL scriptedItem = new ScriptedURL(cursor.getString(urlColumn),
+          cursor.getInt(readColumn), cursor.getString(titleColumn),
+          cursor.getString(contentColumn), cursor.getString(repImageUrlColumn),
+          cursor.getInt(expectedTimeColumn));
       resultList.add(scriptedItem);
     }
 
